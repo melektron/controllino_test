aa92c8376e31c5cb035817d838b4d32bf13c6a2b
melektron <matteo@elektron.work>
2023-04-18 11:08:10 +0200

    added relay blinking

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <Controllino.h>
 
 void setup() {
   // put your setup code here, to run once:
+  for (int i = 0; i < 10; i++)
+  {
+    pinMode(CONTROLLINO_R0 + i, OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(CONTROLLINO_R0 + i, 1);
+    delay(100);
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(CONTROLLINO_R0 + i, 0);
+    delay(100);
+  }
 }
